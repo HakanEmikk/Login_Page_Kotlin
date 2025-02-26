442d7132329522613992d1046492189ab6981214
Hakan Emik <hkn.emk51@gmail.com>
2025-02-26 16:11:51 +0300

    login page comleted

--- app/src/main/java/com/example/loginui/MainActivity.kt
+++ app/src/main/java/com/example/loginui/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.example.loginui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.loginui.ui.theme.LoginUiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +40,65 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LoginUiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Surface(color = MaterialTheme.colorScheme.background) { Greeting() }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        val tfKullaniciAdi = remember { mutableStateOf("") }
+        val tfSifre = remember { mutableStateOf("") }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(painter = painterResource(id = R.drawable.logo), contentDescription = "")
+            TextField(
+                value = tfKullaniciAdi.value,
+                onValueChange = { tfKullaniciAdi.value = it },
+                label = { Text(text = "Kullanıcı Adı") },
+                colors = TextFieldDefaults.colors(
+                    disabledLabelColor = Color.White,
+                    disabledTextColor = Color.Black,
+                    focusedIndicatorColor = Color.Red
+                )
+            )
+            TextField(
+                value = tfSifre.value,
+                onValueChange = { tfSifre.value = it },
+                label = { Text(text = "Şifre") },
+                colors = TextFieldDefaults.colors(
+                    disabledLabelColor = Color.White,
+                    disabledTextColor = Color.Black,
+                    focusedIndicatorColor = Color.Red
+                )
+            )
+            Button(
+                onClick = { Log.e("Giriş", "Başarılı") },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.size(250.dp,50.dp)
+            ) {
+                Text(text = "Şifre")
+            }
+
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LoginUiTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
