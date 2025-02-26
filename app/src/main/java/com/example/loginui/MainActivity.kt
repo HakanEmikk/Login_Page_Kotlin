d6cbaf7850b2423ada3fce725c1964ce5fdb6146
Hakan Emik <hkn.emk51@gmail.com>
2025-02-26 16:22:41 +0300

    login page update

--- app/src/main/java/com/example/loginui/MainActivity.kt
+++ app/src/main/java/com/example/loginui/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,6 +31,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.loginui.ui.theme.LoginUiTheme
@@ -77,10 +80,12 @@ fun Greeting() {
                     disabledLabelColor = Color.White,
                     disabledTextColor = Color.Black,
                     focusedIndicatorColor = Color.Red
-                )
+                ),
+                visualTransformation = PasswordVisualTransformation(),
+
             )
             Button(
-                onClick = { Log.e("Giriş", "Başarılı") },
+                onClick = { Log.e("Giriş", "Basarili") },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Red,
                     contentColor = Color.White
